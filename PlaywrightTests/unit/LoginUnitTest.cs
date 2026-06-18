9d9a41b7f349c2897c4f6cc88540eb141fd417c0
Pablo Vergara <vergara.pablo0616@gmail.com>
2026-06-18 17:38:54 -0400

    go

--- PlaywrightTests/unit/LoginUnitTest.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a864416ce5a54627a4d0b1a84887df5150fcde32280196367fcde61543d846e6
-size 3823
--- PlaywrightTests/unit/UnitTest1.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9eb934c6c6aa65272021d0c9742a2feb61b05ca7c4b88aa2c31aff774bda869c
-size 889
